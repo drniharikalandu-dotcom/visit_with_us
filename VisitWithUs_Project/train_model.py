aa990a62d6b3b9129e9d26398a6540d3c9ca449f
Niharika <drniharikalandu@gmail.com>
2026-06-16 12:40:16 +0530

    Update train_model.py

--- VisitWithUs_Project/train_model.py
+++ VisitWithUs_Project/train_model.py
@@ -1,5 +1,4 @@
-# ✅ Correct imports
-import pandas as pd
+mport pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import joblib
